78443ebc959d1684d7c115a77d5b5005a9eb6871
k756427189-droid <k756427189@163.com>
2026-09-03 13:17:01 +0800

    Update app.js

--- app.js
+++ app.js
@@ -130,4 +130,5 @@ app.post('/api/checkout', async (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+module.exports = app;
 });
